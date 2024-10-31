9ec7ec6c22a2d6f12468031cb0f95f3ccbdd1188
Milos Kotlar <kotlarmilos@gmail.com>
2024-10-31 16:53:23 +0100

    Allow build without sudo privileges

--- eng/restore-toolset.ps1
+++ eng/restore-toolset.ps1
@@ -1,24 +1,29 @@
 #!/usr/bin/env pwsh
 
 function Install-Workloads {
-    # Check if dotnet is installed
-    if (-not (Get-Command dotnet -ErrorAction SilentlyContinue)) {
-        Write-Output "dotnet could not be found. Aborting..."
-        exit 1
-    }
-    else {
-        Write-Output "dotnet is installed at $(Get-Command dotnet)."
+    if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        Write-Host "No sudo privileges detected. Skipping dotnet workload install..."
+    } else {
+        # Check if dotnet is installed
+        if (-not (Get-Command dotnet -ErrorAction SilentlyContinue)) {
+            Write-Output "dotnet could not be found. Aborting..."
+            exit 1
+        }
+        else {
+            Write-Output "dotnet is installed at $(Get-Command dotnet)."
+        }
+
+        # This workload requires Xcode 15.4
+        dotnet workload install maccatalyst --version 9.0.100-rc.1.24453.3 --source https://api.nuget.org/v3/index.json
+
+        if ($LASTEXITCODE -ne 0) {
+            Write-Output "Failed to install workloads."
+            exit 1
+        }
+
+        Write-Output "Workloads installed successfully."
     }
 
-    # This workload requires Xcode 15.4
-    dotnet workload install maccatalyst --version 9.0.100-rc.1.24453.3 --source https://api.nuget.org/v3/index.json
-
-    if ($LASTEXITCODE -ne 0) {
-        Write-Output "Failed to install workloads."
-        exit 1
-    }
-
-    Write-Output "Workloads installed successfully."
     return 0
 }
 
--- eng/restore-toolset.sh
+++ eng/restore-toolset.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
 
 function InstallWorkloads {
-    # Check if dotnet is installed
-    if ! command -v dotnet &> /dev/null
-    then
-        echo "dotnet could not be found. Aborting..."
-        exit 1
+    if [[ "$(id -u)" -ne 0 ]]; then
+        echo "No sudo privileges detected. Skipping dotnet workload install..."
     else
-        echo "dotnet is installed at $(command -v dotnet)."
+        # Check if dotnet is installed
+        if ! command -v dotnet &> /dev/null
+        then
+            echo "dotnet could not be found. Aborting..."
+            exit 1
+        else
+            echo "dotnet is installed at $(command -v dotnet)."
+        fi
+
+        # This workload requires Xcode 15.4
+        dotnet workload install maccatalyst --version 9.0.100-rc.1.24453.3 --source https://api.nuget.org/v3/index.json
+
+        if [[ $? != 0 ]]; then
+            Write-PipelineTelemetryError -category 'InitializeToolset' "Failed to install workloads."
+            ExitWithExitCode 1
+        fi
+
+        echo "Workloads installed successfully."
     fi
-
-    # This workload requires Xcode 15.4
-    dotnet workload install maccatalyst --version 9.0.100-rc.1.24453.3 --source https://api.nuget.org/v3/index.json
-
-    if [[ $? != 0 ]]; then
-        Write-PipelineTelemetryError -category 'InitializeToolset' "Failed to install workloads."
-        ExitWithExitCode 1
-    fi
-
-    echo "Workloads installed successfully."
 
     return 0
 }
